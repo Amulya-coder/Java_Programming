0ea9d291347b7f75d42771e789f245245ab4656e
Amulya Dixit <amulyadixit07@gmail.com>
2022-02-17 16:07:33 +0530

    Check whether a given number is prime or not

---
+++ Java_BasicPrograms/PrimeCheck.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class PrimeCheck {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int flag = 0;
+        if (n == 0 || n == 1) {
+            System.out.println("It is not a Prime number");
+        } else {
+            // int m = n/2;
+
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if (flag == 1) {
+                System.out.println("It is not a prime number");
+            } else {
+                System.out.println("The given number is prime number");
+            }
+        }
+
+    }
+}
