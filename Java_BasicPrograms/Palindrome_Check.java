8642ce83fd441550cbe1b4dcc2b2c47a30a6d0a6
Amulya Dixit <amulyadixit07@gmail.com>
2022-02-17 16:10:20 +0530

    Palindrome

---
+++ Java_BasicPrograms/Palindrome_Check.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class PalindromeCheck {
+
+    public static void main(String[] args) {
+        /** Only for number **/
+        // int n;
+        // Scanner sc=new Scanner(System.in);
+        // n=sc.nextInt();
+        //
+        // int sum=0,temp,r;
+        // temp=n;
+        //
+        // while(n>0){
+        // r=n%10;
+        // sum=(sum*10)+r;
+        // n=n/10;
+        // }
+        //
+        // if(sum==temp){
+        // System.out.println("Given number is palindrome");
+        // }
+        // else{
+        // System.out.println("Given number is not palindrome");
+        // }
+
+        /** for both number and string **/
+        String original, reverse = "";
+        Scanner sc = new Scanner(System.in);
+        original = sc.nextLine();
+        int len = original.length();
+
+        for (int i = len - 1; i >= 0; i--) {
+            reverse = reverse + original.charAt(i);
+        }
+        if (reverse.equals(original)) {
+            System.out.println("String/number is palindrome");
+        } else {
+            System.out.println("String/number is not palindrome");
+        }
+    }
+}
